a464af303876e44f04d7b29984c915904924c38a
colewortman <wortcole12@gmail.com>
2024-07-11 14:58:56 -0500

    created and tested extract_markdown_images and extract_markdown_links functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -6,6 +6,8 @@ from textnode import (
     text_type_code
 )
 
+import re
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
 
@@ -30,5 +32,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
         
-            
-        
+def extract_markdown_images(text):
+    return (re.findall(r"!\[(.*?)\]\((.*?)\)", text))
+
+def extract_markdown_links(text):
+    return (re.findall(r"\[(.*?)\]\((.*?)\)", text))
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,9 @@
 import unittest
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links
+)
 
 from textnode import (
     TextNode,
@@ -88,7 +92,26 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+    
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            extract_markdown_images(text)
+        )
 
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            extract_markdown_links(text)
+        )
 
 if __name__ == "__main__":
     unittest.main()
