f474e47e021367d12d47db5ff0f7855cd7a02007
colewortman <wortcole12@gmail.com>
2024-07-18 13:57:16 -0500

    Created and tested markdown_blocks.py

---
+++ src/test_markdown_blocks.py
@@ -0,0 +1,15 @@
+import unittest
+
+from markdown_blocks import markdown_to_blocks
+
+class TestMarkdownBlocks(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        markdown = "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+        self.assertListEqual(
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
+            ],
+            markdown_to_blocks(markdown)
+        )
