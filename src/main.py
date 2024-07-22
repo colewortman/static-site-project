47081cf0171dd9998af7a53061501f84818e80ef
colewortman <wortcole12@gmail.com>
2024-07-21 22:02:09 -0500

    Created a function to copy a directory and its contents to overwrite another directory

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 from textnode import TextNode
+import os
+import shutil
 
 def main():
     new_node = TextNode('this is a text node', 'bold', 'https://www.boot.dev')
     print(new_node)
+    copy_contents("/home/colewortman/workspace/github.com/colewortman/static-site-project/static", "/home/colewortman/workspace/github.com/colewortman/static-site-project/public")
+
+def copy_contents(copy_path, target_path):
+    if os.path.exists(target_path):
+        shutil.rmtree(target_path)
+    os.makedirs(target_path)
+
+    for item in os.listdir(copy_path):
+        source_item_path = os.path.join(copy_path, item)
+        target_item_path = os.path.join(target_path, item)
+
+        if os.path.isfile(source_item_path):
+            shutil.copy(source_item_path, target_item_path)
+        elif os.path.isdir(source_item_path):
+            copy_contents(source_item_path, target_item_path)
+        
+        print(f"Copied {source_item_path} to {target_item_path}")
 
 main()
